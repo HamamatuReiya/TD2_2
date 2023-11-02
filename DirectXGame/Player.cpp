187a450d73a9876467756c33e3ea84c3d16dd06d
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2023-11-02 18:56:55 +0900

    途中

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -10,7 +10,7 @@ void Player::Initialize(Model* model) {
 	worldTransform_.Initialize();
 	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
 	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_.translation_ = {0.0f, 0.0f, 30.0f};
 }
 
 void Player::Update() {
