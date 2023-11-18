d3fcb1c71881995c732d2e84a2ade0be8dcf7b99
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-11-19 02:18:55 +0900

    途中

---
+++ DirectXGame/Table.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+class Table {
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(
+	    Model* model, Model* model1, Model* model2, Model* model3, Model* model4, Model* model5,
+	    Model* model6, Model* model7, Model* model8);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ViewProjection& viewProjection);
+
+private:
+	WorldTransform worldTransform_[9];
+	Model* model_ = nullptr;
+	Model* model1_ = nullptr;
+	Model* model2_ = nullptr;
+	Model* model3_ = nullptr;
+	Model* model4_ = nullptr;
+	Model* model5_ = nullptr;
+	Model* model6_ = nullptr;
+	Model* model7_ = nullptr;
+	Model* model8_ = nullptr;
+};
