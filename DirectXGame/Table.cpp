6226594f09421d88abf0514929eb829ad9d93bc3
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-11-20 02:10:37 +0900

    台設置

--- DirectXGame/Table.cpp
+++ DirectXGame/Table.cpp
@@ -17,19 +17,19 @@ void Table::Initialize(
 		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
 		worldTransform_[i].rotation_ = {0.0f, 0.0f, 0.0f};
 	}
-	worldTransform_[0].translation_ = {-1.0f, 0.0f, -1.0f};
-	worldTransform_[1].translation_ = {-1.0f, 0.0f, 0.0f};
-	worldTransform_[2].translation_ = {-1.0f, 0.0f, 1.0f};
-	worldTransform_[3].translation_ = {0.0f, 0.0f, -1.0f};
-	worldTransform_[4].translation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_[5].translation_ = {0.0f, 0.0f, 1.0f};
-	worldTransform_[6].translation_ = {1.0f, 0.0f, -1.0f};
-	worldTransform_[7].translation_ = {1.0f, 0.0f, 0.0f};
-	worldTransform_[8].translation_ = {1.0f, 0.0f, 1.0f};
+	worldTransform_[0].translation_ = {13.5f, 0.0f, 19.4f};
+	worldTransform_[1].translation_ = {32.4f, 0.0f, 40.8f};
+	worldTransform_[2].translation_ = {11.3f, 0.0f, 67.65f};
+	worldTransform_[3].translation_ = {41.5, 0.0f, 9.45f};
+	worldTransform_[4].translation_ = {75.6f, 0.0f, -11.95f};
+	worldTransform_[5].translation_ = {52.8f, 0.0f, 81.35f};
+	worldTransform_[6].translation_ = {11.7f, 0.0f, -8.65f};
+	worldTransform_[7].translation_ = {7.3f, 0.0f, 6.0f};
+	worldTransform_[8].translation_ = {25.6f, 0.0f, 80.85f};
 }
 
 void Table::Update() {
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 9; i++) {
 		// 行列の更新
 		worldTransform_[i].UpdateMatrix();
 	}
