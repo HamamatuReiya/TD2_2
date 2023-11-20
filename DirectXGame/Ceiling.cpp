952d31376c085001cfebeca94c4f3748beb32d63
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-11-21 00:46:10 +0900

    天井を追加

---
+++ DirectXGame/Ceiling.cpp
@@ -0,0 +1,19 @@
+﻿#include "Ceiling.h"
+
+void Ceiling::Initialize(Model* model) {
+	model_ = model;
+	worldTransform_.Initialize();
+	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
+	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_.translation_ = {40.0f, 2.5f, 36.0f};
+}
+
+void Ceiling::Update() {
+	// 行列の更新
+	worldTransform_.UpdateMatrix();
+}
+
+void Ceiling::Draw(ViewProjection& viewProjection) {
+
+	model_->Draw(worldTransform_, viewProjection);
+}
